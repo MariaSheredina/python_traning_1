e6befd65fd4726a855f60ac319931a270c753a12
LAPTOP-DINNEV1N\Maria <Maria.Sheredina@bercut.com>
2022-07-25 02:01:12 +0300

    commit_задание3_испр1_25.07_class_contact

---
+++ contact.py
@@ -0,0 +1,13 @@
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, company, address, mobile, bday, bmonth, byear):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.company = company
+        self.address = address
+        self.mobile = mobile
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
