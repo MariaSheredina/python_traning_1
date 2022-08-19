2ecbb3e49c43bc3a66cf34587103eb295d76636e
LAPTOP-DINNEV1N\Maria <Maria.Sheredina@bercut.com>
2022-08-19 18:23:49 +0300

    20.08_урок3_3_не работает len

--- test/test_edit_contact.py
+++
@@ -1,5 +0,0 @@
-# push_задание7
-# -*- coding: utf-8 -*-
-
-def test_edit_group(app):
-    app.contact.edit_first_contact(new_address="Moscow")
--- test/test_edit_group.py
+++
@@ -1,5 +0,0 @@
-# push_задание7
-# -*- coding: utf-8 -*-
-
-def test_edit_group(app):
-    app.group.edit_first_group(new_name="qqq")
