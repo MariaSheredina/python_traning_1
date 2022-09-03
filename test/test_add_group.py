4bdc18cfba1fe3cb55a82337e3498d2b4cef0f58
LAPTOP-DINNEV1N\Maria <Maria.Sheredina@bercut.com>
2022-09-04 00:51:47 +0300

    04.09_хеширование для групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -8,7 +8,7 @@ def test_add_group(app):
     old_groups = app.group.get_group_list()
     group = Group(name="n1", header="h1", footer="f1")
     app.group.create_group(group)
+    assert len(old_groups) + 1 == app.group.count_group()
     new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
