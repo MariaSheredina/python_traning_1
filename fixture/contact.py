df74576f5b0960d74d4fd629fe689628fc04826e
LAPTOP-DINNEV1N\Maria <Maria.Sheredina@bercut.com>
2022-09-04 01:00:04 +0300

    04.09_кэширование и хеширование для контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -15,6 +15,7 @@ class ContactHelper:
         self.fill_contact_form(contact)
         wd.find_element_by_name("submit").click()
         self.app.open.contact_page()
+        self.contact_cache = None
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
@@ -39,6 +40,7 @@ class ContactHelper:
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to.alert.accept()
         self.app.open.home_page()
+        self.contact_cache = None
 
     def modify_first_contact(self, new_contact_data):
         wd = self.app.wd
@@ -48,6 +50,7 @@ class ContactHelper:
         self.fill_contact_form(new_contact_data)
         wd.find_element_by_name("update").click()
         self.app.open.home_page()
+        self.contact_cache = None
 
     def select_first_contact(self):
         wd = self.app.wd
@@ -58,14 +61,17 @@ class ContactHelper:
         self.app.open.home_page()
         return len(wd.find_elements_by_name("selected[]"))
 
+    contact_cache = None
+
     def get_contact_list(self):
-        wd = self.app.wd
-        self.app.open.home_page()
-        list_contact = []
-        for element in wd.find_elements_by_name("entry"):
-            cells = element.find_elements_by_tag_name("td")
-            id = cells[0].find_element_by_name("selected[]").get_attribute("value")
-            fn = cells[2].text
-            ln = cells[1].text
-            list_contact.append(Contact(id=id, firstname=fn, lastname=ln))
-        return list_contact
+        if self.contact_cache is None:
+            wd = self.app.wd
+            self.app.open.home_page()
+            self.contact_cache = []
+            for element in wd.find_elements_by_name("entry"):
+                cells = element.find_elements_by_tag_name("td")
+                id = cells[0].find_element_by_name("selected[]").get_attribute("value")
+                fn = cells[2].text
+                ln = cells[1].text
+                self.contact_cache.append(Contact(id=id, firstname=fn, lastname=ln))
+        return list(self.contact_cache)
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -7,7 +7,7 @@ def test_contact(app):
     old_contact = app.contact.get_contact_list()
     contact = Contact(firstname="Иван", lastname="Иванов")
     app.contact.create_contact(contact)
+    assert len(old_contact) + 1 == app.contact.count_contact()
     new_contact = app.contact.get_contact_list()
-    assert len(old_contact) + 1 == len(new_contact)
     old_contact.append(contact)
     assert sorted(old_contact, key=Contact.id_or_max) == sorted(new_contact, key=Contact.id_or_max)
